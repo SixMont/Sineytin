fde57d77c7351e2ee0b98daea77a77e9cd2beb7b
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2022-11-29 14:42:28 +0100

    Update Code_Joystick.c

--- Code_Joystick.c
+++ Code_Joystick.c
@@ -1,7 +1,7 @@
 
 
 //Dessin de carre
-//a mettre dans le fichier 
+//a mettre dans le fichier stm32f1_ili9341.c
 
 void ILI9341_DrawSquare(int16_t x0, int16_t y0, int16_t taille, uint16_t color){ //x0 et y0 coresponde au centre du carré
 	for(int i=0; i<5;i++){
