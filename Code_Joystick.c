af57b222f605ef8e85a0cbcb37570d8fe082d304
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2022-11-29 14:41:27 +0100

    Create Code_Joystick.c

---
+++ Code_Joystick.c
@@ -0,0 +1,18 @@
+
+
+//Dessin de carre
+//a mettre dans le fichier 
+
+void ILI9341_DrawSquare(int16_t x0, int16_t y0, int16_t taille, uint16_t color){ //x0 et y0 coresponde au centre du carré
+	for(int i=0; i<5;i++){
+		ILI9341_DrawLine(x0+taille/2+i,y0,x0+taille/2+i,y0+taille/2+i,color); // On dessine la moitiée du coté gauche
+		ILI9341_DrawLine(x0+taille/2+i,y0,x0+taille/2+i,y0-taille/2-i,color); // On dessine l'autre moitiée du coté gauche
+		ILI9341_DrawLine(x0-taille/2-i,y0,x0-taille/2-i,y0+taille/2+i,color);
+		ILI9341_DrawLine(x0-taille/2-i,y0,x0-taille/2-i,y0-taille/2-i,color);
+		ILI9341_DrawLine(x0,y0+taille/2+i,x0+taille/2+i,y0+taille/2+i,color);
+		ILI9341_DrawLine(x0,y0+taille/2+i,x0-taille/2-i,y0+taille/2+i,color);
+		ILI9341_DrawLine(x0,y0-taille/2-i,x0+taille/2+i,y0-taille/2-i,color);
+		ILI9341_DrawLine(x0,y0-taille/2-i,x0-taille/2-i,y0-taille/2-i,color);
+
+	}
+}
