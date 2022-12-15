091696d7448700bb91051a1a192a97a95cef0214
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2022-12-15 11:55:30 +0100

    Update Menu.c

--- Graphique/Menu.c
+++ Graphique/Menu.c
@@ -3,7 +3,34 @@
 
 //fonctions ecran selection
 
-int main(void)
-{
-  
+void Ecran_titre_jeu(void){
+	FOND_Espace();
+	ILI9341_Puts(55,100, "SINEYTIN", &Font_16x26, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+	ILI9341_Puts(95,200, "Start", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+}
+
+void Ecran_Selection(void){
+	FOND_Espace();
+	ILI9341_Puts(65,100, "Mode Solo", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+	ILI9341_Puts(45,200, "Mode 2 Joueurs", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+	int i=0;
+	while(1){
+
+		if (JOYSTICK_y_getValue()>2200){
+			i=1;
+		}
+		if(i==-1){
+			ILI9341_Puts(45,200, "Mode 2 Joueurs", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
+			HAL_Delay(50);
+			ILI9341_Puts(45,200, "Mode 2 Joueurs", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+		}
+		if (JOYSTICK_y_getValue()<1520){
+			i=-1;
+		}
+		if (i==1){
+			ILI9341_Puts(65,100, "Mode Solo", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
+			HAL_Delay(200);
+			ILI9341_Puts(65,100, "Mode Solo", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+		}
+	}
 }
