597913ad2358ff7235a5eeed225adb804959635b
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2023-01-03 17:09:44 +0100

    Update mainQuentin.c

--- PasFini/mainQuentin.c
+++ PasFini/mainQuentin.c
@@ -28,6 +28,49 @@ void process_ms(void)
 static int xJoueur=120;
 static int yJoueur=20;
 
+//Diagramme d'etat
+void FOND_Espace(void){
+	int x=0;
+	int y=340;
+	ILI9341_Fill(ILI9341_COLOR_BLACK);
+	while (y>20){
+		x=rand();
+		ILI9341_DrawPixel(x,y,ILI9341_COLOR_WHITE);
+		y--;
+	}
+}
+
+void Ecran_titre_jeu(void){
+	FOND_Espace();
+	ILI9341_Puts(55,100, "SINEYTIN", &Font_16x26, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+	ILI9341_Puts(95,200, "Start", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+}
+
+void Ecran_Selection(void){
+	FOND_Espace();
+	ILI9341_Puts(65,100, "Mode Solo", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+	ILI9341_Puts(45,200, "Mode 2 Joueurs", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+	int i=0;
+	while(1){
+
+		if (JOYSTICK_y_getValue()>2200){
+			i=1;
+		}
+		if(i==-1){
+			ILI9341_Puts(65,100, "Mode Solo", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+			ILI9341_Puts(45,200, "Mode 2 Joueurs", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
+		}
+		if (JOYSTICK_y_getValue()<1520){
+			i=-1;
+		}
+		if (i==1){
+			ILI9341_Puts(45,200, "Mode 2 Joueurs", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
+			ILI9341_Puts(65,100, "Mode Solo", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
+
+		}
+	}
+}
+// jusque la pour le diagramme d'état.
 void JOYSTICK_move_x(void){
 	while(1){
 		if (JOYSTICK_x_getValue()>2200){
