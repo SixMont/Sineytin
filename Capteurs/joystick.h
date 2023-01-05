0d40fdef928a10a4481be3a0b1440dccfd74390f
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2023-01-05 09:08:14 +0100

    Update joystick.h

--- Capteurs/joystick.h
+++ Capteurs/joystick.h
@@ -3,7 +3,11 @@
 
 void JOYSTICK_x_init(void);
 void JOYSTICK_y_init(void);
+void JOYSTICK2_x_init(void);
 
 uint16_t JOYSTICK_x_getValue();
+uint16_t JOYSTICK_y_getValue();
+uint16_t JOYSTICK2_x_getValue();
+
 
 #endif
