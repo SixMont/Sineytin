ab57c8490a233e27ee96edf0b17db3cc3a43e94d
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2023-01-05 09:07:41 +0100

    Update joystick.c

--- Capteurs/joystick.c
+++ Capteurs/joystick.c
@@ -5,8 +5,10 @@
 #include "stm32f1_gpio.h"
 
 
-static volatile uint16_t positionX;
-
+static volatile uint16_t position1y;
+static volatile uint16_t position2y;
+static volatile uint16_t position1x;
+static volatile uint16_t position2x;
 
 
 void JOYSTICK_x_init(void){
@@ -19,7 +21,23 @@ void JOYSTICK_y_init(void){
 	ADC_init();
 }
 
+uint16_t JOYSTICK_y_getValue(){
+	position1x=ADC_getValue(ADC_2);
+	return position1x;
+}
+
+
 uint16_t JOYSTICK_x_getValue(){
-	positionX=ADC_getValue(ADC_3);
-	return positionX;
+	position1y=ADC_getValue(ADC_3);
+	return position1y;
+}
+
+void JOYSTICK2_x_init(void){
+	//BSP_GPIO_PinCfg(JOYSTICK2_x_PORT, JOYSTICK2_x_PIN, GPIO_MODE_INPUT, GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH);
+	ADC_init();
+}
+
+uint16_t JOYSTICK2_x_getValue(){
+	position2x=ADC_getValue(ADC_11);
+	return position2x;
 }
