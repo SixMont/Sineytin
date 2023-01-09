a9a25676cbb2b244a025f7be9467419e816ed76b
Bryan3012 <105267997+Bryan3012@users.noreply.github.com>
2023-01-09 15:09:23 +0100

    Update CodeDetection.c

--- CodeVianney/CodeDetection.c
+++ CodeVianney/CodeDetection.c
@@ -1,3 +1,10 @@
+static uint16_t yEnnemi = 240;
+static uint16_t xEnnemi = 240;
+uint16_t r=4;
+static bool_e tir = FALSE;
+static uint16_t y = 20;
+
+
 bool_e collision(uint16_t xc,uint16_t y,uint16_t r){
 
 
@@ -5,8 +12,8 @@ bool_e collision(uint16_t xc,uint16_t y,uint16_t r){
 	 * détecte si la distance entre centre du cercle et centre du carré est inférieure
 	 * au rayon du cercle et la moitié du côté du carré
 	 */
-	uint32_t distance = sqrt(square((200-xc))+square((300-y)));
-	if (distance<=(15)){
+	uint32_t distance = sqrt(square((xEnnemi-xc))+square((yEnnemi-y)));
+	if (distance<=(r+4)){
 
 
 		return TRUE;
@@ -14,3 +21,43 @@ bool_e collision(uint16_t xc,uint16_t y,uint16_t r){
 	}
 
 	return FALSE;
+
+}
+
+
+int main(void)
+{
+
+			if(readButton() || tir==TRUE ){
+
+			if(tir==FALSE){
+				xc=xJoueur;
+				tir=TRUE;
+
+
+			}
+
+
+			shot(xc,y);
+
+			if(y>=310){
+
+				tir=FALSE;
+
+				ILI9341_DrawFilledCircle(xc, y+2,r+2, ILI9341_COLOR_WHITE);
+				y=20;
+			}
+
+
+
+			if(collision(xc,y,10)){
+
+
+				ILI9341_DrawFilledRectangle(xEnnemi-5,yEnnemi-5,xEnnemi+2,yEnnemi+2,ILI9341_COLOR_WHITE);
+				ILI9341_DrawFilledCircle(xc, y+2,r+2, ILI9341_COLOR_WHITE);
+				tir=FALSE;
+				y=20;
+			}
+			y++;
+
+			}
