c6c42edc7fe225cfe258665231d1ac216bd92547
Quentin56S49 <119422250+Quentin56S49@users.noreply.github.com>
2023-01-09 16:25:54 +0100

    Update mainQuentin.c

--- PasFini/mainQuentin.c
+++ PasFini/mainQuentin.c
@@ -7,6 +7,11 @@
 #include "systick.h"
 #include "tft_ili9341/stm32f1_ili9341.h"
 #include "joystick.h"
+#include <math.h>
+
+#define square(a) (a)*(a)
+
+
 
 void writeLED(bool_e b)
 {
@@ -15,7 +20,7 @@ void writeLED(bool_e b)
 
 bool_e readButton(void)
 {
-	return !HAL_GPIO_ReadPin(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN);
+	return !HAL_GPIO_ReadPin(BUTTON_U_GPIO, BUTTON_U_PIN);
 }
 
 static volatile uint32_t t = 0;
@@ -24,11 +29,87 @@ void process_ms(void)
 	if(t)
 		t--;
 }
-
 static int xJoueur=120;
-static int yJoueur=20;
+static int yJoueur=300;
+static int xJoueur2=120;
+static int yJoueur2=20;
+uint16_t r=4;
+static bool_e tir = FALSE;
+static uint16_t y = 20;
 
-//Diagramme d'etat
+
+/*static void move(){
+	bool_e allerDroite=FALSE;
+	bool_e allerGauche=FALSE;
+	while(1){
+		if (JOYSTICK_x_getValue()>2944){
+			allerDroite=FALSE;
+			allerGauche=TRUE;
+		}else if (JOYSTICK_x_getValue()<1500){
+			allerGauche=FALSE;
+			allerDroite=TRUE;
+		}else{
+			allerGauche=FALSE;
+			allerDroite=FALSE;
+		}
+
+		while (allerGauche){
+			xJoueur++;
+			ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_WHITE);
+			ILI9341_DrawSquare(xJoueur+1,yJoueur,5,ILI9341_COLOR_BLUE);
+			if (xJoueur==220){
+				ILI9341_DrawSquare(xJoueur+1,yJoueur,5,ILI9341_COLOR_WHITE);
+				allerGauche=FALSE;
+				allerDroite=FALSE;
+			}
+		}
+		while (allerDroite){
+			xJoueur--;
+			ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_WHITE);
+			ILI9341_DrawSquare(xJoueur-1,yJoueur,5,ILI9341_COLOR_BLUE);
+			if (xJoueur==20){
+				ILI9341_DrawSquare(xJoueur-1,yJoueur,5,ILI9341_COLOR_WHITE);
+				allerDroite=FALSE;
+				allerGauche=FALSE;
+			}
+		}
+	}
+
+}*/
+
+/*static void Test_Deplacement(){
+	while(1)	//boucle de tâche de fond
+		{
+		if (JOYSTICK_x_getValue()>2200){
+			while (xJoueur<220){
+				ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_WHITE);
+				ILI9341_DrawSquare(120,120,5,ILI9341_COLOR_WHITE);
+				ILI9341_DrawSquare(xJoueur+10,yJoueur,5,ILI9341_COLOR_RED);
+				xJoueur+=10;
+				}
+		}
+		if (JOYSTICK_x_getValue()<1500){
+			while (xJoueur>20){
+				ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_WHITE);
+				ILI9341_DrawSquare(120,120,5,ILI9341_COLOR_WHITE);
+				ILI9341_DrawSquare(xJoueur-10,yJoueur,5,ILI9341_COLOR_BLUE);
+				xJoueur-=10;
+			}
+		}
+
+		if (JOYSTICK_x_getValue()<2200 && JOYSTICK_x_getValue()>1500){
+				ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_WHITE);
+				ILI9341_DrawSquare(120,120,5,ILI9341_COLOR_GREEN);
+				xJoueur=120;
+		}
+	}
+}*/
+
+/*Fonction qui vas premettre de lancer l'écran titre
+ * Fond étoiler
+ * Titre du jeu
+ * Bouton start
+ */
 void FOND_Espace(void){
 	int x=0;
 	int y=340;
@@ -37,6 +118,52 @@ void FOND_Espace(void){
 		x=rand();
 		ILI9341_DrawPixel(x,y,ILI9341_COLOR_WHITE);
 		y--;
+	}
+}
+
+bool_e Mode_2_Joueurs(void){
+	uint32_t xc;
+	uint32_t y;
+	int life1=3;
+	int life2=3;
+	int life1Before=3;
+	int life2Before=3;
+	drawLife(life1,ILI9341_COLOR_BLUE);
+	drawLife2(life1,ILI9341_COLOR_RED);
+	while (life1!=0 || life2!= 0){
+		JOYSTICK_move_x();
+		JOYSTICK2_move_x();
+		if(readButton() || tir==TRUE ){
+			if(tir==FALSE){
+				xc=xJoueur;
+				tir=TRUE;
+			}
+			shot(xc,y);
+			if(y<=20){
+					tir=FALSE;
+				ILI9341_DrawFilledCircle(xc, y+2,r+2, ILI9341_COLOR_WHITE);
+				y=300;
+				}
+				if (collision(xc,y,10)){
+					tir=FALSE;
+					y=300;
+					life1--;
+				}
+				if (life1<life1Before){
+					drawLife(life1,ILI9341_COLOR_BLUE);
+				life1Before=life1;
+				}
+				if (life2<life2Before){
+					drawLife2(life2,ILI9341_COLOR_RED);
+					life2Before=life2;
+				}
+		}
+	}
+	if (life1==0){
+		return TRUE;
+	}
+	if (life2==0){
+		return FALSE;
 	}
 }
 
@@ -71,15 +198,15 @@ void Ecran_Selection(void){
 		}
 	}
 }
-// jusque la pour le diagramme d'état.
+
+
+
 void JOYSTICK_move_x(void){
-	while(1){
-		if (JOYSTICK_x_getValue()>2200){
-			JOYSTICK_move_x_GAUCHE();
-		}
-		if (JOYSTICK_x_getValue()<1520){
-			JOYSTICK__move_x_DROITE();
-		}
+	if (JOYSTICK_x_getValue()>2200){
+		JOYSTICK__move_x_DROITE();
+	}
+	if (JOYSTICK_x_getValue()<1520){
+		JOYSTICK_move_x_GAUCHE();
 	}
 }
 
@@ -100,6 +227,79 @@ void JOYSTICK_move_x_GAUCHE(void){
 	}
 }
 
+
+void JOYSTICK2_move_x(void){
+	if (JOYSTICK2_x_getValue()<1000){
+		JOYSTICK2_move_x_GAUCHE();
+	}
+	if (JOYSTICK2_x_getValue()>2200){
+		JOYSTICK2_move_x_DROITE();
+	}
+}
+
+
+void JOYSTICK2_move_x_DROITE(void){
+	if (xJoueur2>20){
+		ILI9341_DrawSquare(xJoueur2,yJoueur2,5,ILI9341_COLOR_BLACK);
+		ILI9341_DrawSquare(xJoueur2-5,yJoueur2,5,ILI9341_COLOR_RED);
+		xJoueur2-=5;
+	}
+}
+
+void JOYSTICK2_move_x_GAUCHE(void){
+	if (xJoueur2<220){
+		ILI9341_DrawSquare(xJoueur2,yJoueur2,5,ILI9341_COLOR_BLACK);
+		ILI9341_DrawSquare(xJoueur2+5,yJoueur2,5,ILI9341_COLOR_RED);
+		xJoueur2+=5;
+	}
+}
+
+
+void drawLife(int nbLife, uint16_t color){
+	for (int i=0; i<3 ; i++){
+		ILI9341_DrawFilledRectangle(230, 230-i*100, 228,  270-i*100,ILI9341_COLOR_BLACK );
+	}
+	for (int i=0; i<nbLife ; i++){
+		ILI9341_DrawFilledRectangle(230, 230-i*100, 228,  270-i*100,color);
+	}
+}
+
+void drawLife2(int nbLife, uint16_t color){
+	for (int i=0; i<3 ; i++){
+		ILI9341_DrawFilledRectangle(10, 230-i*100, 20,  270-i*100,ILI9341_COLOR_BLACK );
+	}
+	for (int i=0; i<nbLife ; i++){
+		ILI9341_DrawFilledRectangle(10, 230-i*100, 12,  270-i*100,color);
+	}
+}
+bool_e collision(uint32_t xc,uint32_t y,uint32_t r){
+
+
+	/**
+	 * détecte si la distance entre centre du cercle et centre du carré est inférieure
+	 * au rayon du cercle et la moitié du côté du carré
+	 */
+	uint32_t distance = sqrt(square((xJoueur2-xc))+square((yJoueur2-y)));
+	if (distance<=(r+4)){
+
+
+		return TRUE;
+
+	}
+
+	return FALSE;
+
+}
+void shot(uint16_t xc, uint16_t y){
+
+
+
+
+
+	ILI9341_DrawFilledCircle(xc, y+4,r, ILI9341_COLOR_WHITE);
+	ILI9341_DrawFilledCircle(xc, y-2,r, ILI9341_COLOR_RED);
+
+}
 //Fonction affiche_ennemi à reimplémenter. Disponible dans github.
 
 int main(void)
@@ -132,8 +332,11 @@ int main(void)
 	BSP_GPIO_PinCfg(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN, GPIO_MODE_INPUT,GPIO_PULLUP,GPIO_SPEED_FREQ_HIGH);
 	JOYSTICK_x_init();
 	ILI9341_Init();
+	ILI9341_Rotate(ILI9341_Orientation_Portrait_2);
 	ILI9341_Fill(ILI9341_COLOR_BLACK);
-	ILI9341_DrawCircle(120,20,5,ILI9341_COLOR_BLUE);
-	JOYSTICK_move_x();
-
+	//Ecran_titre_jeu();
+	//Ecran_Selection();
+	//JOYSTICK2_move_x();
+	//JOYSTICK_move_x();
+	Mode_2_Joueurs();
 }
