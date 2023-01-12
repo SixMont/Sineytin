ad95440e4b52bda7f46500c6098f726cfc485dd1
SixMonts <113461402+SixMont@users.noreply.github.com>
2023-01-12 18:18:56 +0100

    Update Code.c

--- Machine_a_etat/Code.c
+++ Machine_a_etat/Code.c
@@ -15,10 +15,19 @@
 #include "systick.h"
 #include "tft_ili9341/stm32f1_ili9341.h"
 #include "joystick.h"
+#include <math.h>
 
-static int xJoueur=120;
-static int yJoueur=20;
+#define square(a) (a)*(a)
+
 static volatile uint32_t t = 0;
+static int xJoueur=120;
+static int yJoueur=300;
+static int xJoueur2=120;
+static int yJoueur2=20;
+uint16_t r=4;
+static bool_e tir = FALSE;
+static bool_e tir2 = FALSE;
+static uint16_t y = 20;
 
 void process_ms(void)
 {
@@ -26,17 +35,44 @@ void process_ms(void)
 		t--;
 }
 
+/*
+ * @Brief modifie la valeur du
+ * @param  Non
+ * @retval Non
+*/
+/*
 void writeBUZZER(bool_e b)
 {
-	HAL_GPIO_WritePin(BUZZER_GPIO, BUZZER_PIN, b);
+	!HAL_GPIO_WritePin(BUZZER_GPIO, BUZZER_PIN, b);
 }
+*/
+/*
+ * @Brief lit la valeur du bouton2
+ * @param  Non
+ * @retval Non
+*/
 
-bool_e readButton1(void)
+bool_e readButton2(void)
 {
 	return !HAL_GPIO_ReadPin(BUTTON1_GPIO, BUTTON1_PIN);
 }
 
-//Fonctions Controle carré
+/*
+ * @Brief lit la valeur du bouton
+ * @param  Non
+ * @retval Non
+*/
+
+bool_e readButton(void)
+{
+	return !HAL_GPIO_ReadPin(BUTTON1_GPIO, BUTTON1_PIN);
+}
+
+/*
+ * @Brief detecte les mouvements du joystick
+ * @param  Non
+ * @retval Non
+*/
 
 void JOYSTICK_move_x(void){
 	while(1){
@@ -49,6 +85,12 @@ void JOYSTICK_move_x(void){
 	}
 }
 
+/*
+ * @Brief decale un carré vers la droite
+ * @param  Non
+ * @retval Non
+*/
+
 void JOYSTICK__move_x_DROITE(void){
 	if (xJoueur>20){
 		ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_BLACK);
@@ -57,11 +99,60 @@ void JOYSTICK__move_x_DROITE(void){
 	}
 }
 
+/*
+ * @Brief decale un carré vers la gauche
+ * @param  Non
+ * @retval Non
+*/
+
 void JOYSTICK_move_x_GAUCHE(void){
 	if (xJoueur<220){
 		ILI9341_DrawSquare(xJoueur,yJoueur,5,ILI9341_COLOR_BLACK);
 		ILI9341_DrawSquare(xJoueur+5,yJoueur,5,ILI9341_COLOR_BLUE);
 		xJoueur+=5;
+	}
+}
+
+/*
+ * @Brief detecte les mouvements du joystick2
+ * @param  Non
+ * @retval Non
+*/
+
+void JOYSTICK2_move_x(void){
+	if (JOYSTICK2_x_getValue()<1000){
+		JOYSTICK2_move_x_GAUCHE();
+	}
+	if (JOYSTICK2_x_getValue()>2200){
+		JOYSTICK2_move_x_DROITE();
+	}
+}
+
+/*
+ * @Brief decale un carré vers la droite
+ * @param  Non
+ * @retval Non
+*/
+
+void JOYSTICK2_move_x_DROITE(void){
+	if (xJoueur2>20){
+		ILI9341_DrawSquare(xJoueur2,yJoueur2,5,ILI9341_COLOR_BLACK);
+		ILI9341_DrawSquare(xJoueur2-5,yJoueur2,5,ILI9341_COLOR_RED);
+		xJoueur2-=5;
+	}
+}
+
+/*
+ * @Brief decale un carré vers la gauche
+ * @param  Non
+ * @retval Non
+*/
+
+void JOYSTICK2_move_x_GAUCHE(void){
+	if (xJoueur2<220){
+		ILI9341_DrawSquare(xJoueur2,yJoueur2,5,ILI9341_COLOR_BLACK);
+		ILI9341_DrawSquare(xJoueur2+5,yJoueur2,5,ILI9341_COLOR_RED);
+		xJoueur2+=5;
 	}
 }
 
@@ -81,7 +172,7 @@ void Ecran_defaite(void){
 
 			ILI9341_Puts(95,200, "Restart", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
 
-			if(readButton1()){
+			if(readButton()){
 				sortir=1;
 
 			}
@@ -104,7 +195,7 @@ void Ecran_victoire(void){
 
 			ILI9341_Puts(95,200, "Restart", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
 
-			if(readButton1()){
+			if(readButton()){
 				sortir=1;
 
 			}
@@ -135,7 +226,7 @@ int Ecran_titre_jeu(void){
 				ILI9341_Puts(95,200, "Start", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
 				ILI9341_Puts(95,280, "leave", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
 
-				if(readButton1()){
+				if(readButton()){
 					sortir=-1;
 				}
 			}
@@ -147,7 +238,7 @@ int Ecran_titre_jeu(void){
 				ILI9341_Puts(95,280, "leave", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
 				ILI9341_Puts(95,200, "Start", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
 
-				if(readButton1()){
+				if(readButton()){
 					sortir=1;
 				}
 			}
@@ -176,7 +267,7 @@ int Ecran_Selection(void){
 		if(i==-1){//mode 2 joueurs
 			ILI9341_Puts(65,100, "Solo", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
 			ILI9341_Puts(45,200, "2 players", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
-			if(readButton1()){
+			if(readButton()){
 											sortir=-1;
 			}
 		}
@@ -187,7 +278,7 @@ int Ecran_Selection(void){
 			ILI9341_Puts(45,200, "2 players", &Font_11x18, ILI9341_COLOR_WHITE,ILI9341_COLOR_BLACK);
 			ILI9341_Puts(65,100, "Solo", &Font_11x18, ILI9341_COLOR_BLACK,ILI9341_COLOR_WHITE);
 
-			if(readButton1()){
+			if(readButton()){
 								sortir=1;
 			}
 
@@ -210,26 +301,24 @@ void FOND_Espace(void){
 		x=rand();
 		ILI9341_DrawPixel(x,y,ILI9341_COLOR_WHITE);
 		y--;
-		
+
 	}
 }
 
 /*
  * @Brief affiche le fond d'ecran
- * @param  xc : coordonnée x centre boule y : coordonnée y centre boule
+ * @param  x : coordonnée x centre boule y : coordonnée y centre boule
  * @retval TRUE : si collision FALSE : si pas collision
 */
 
-bool_e collision(uint16_t x,uint16_t y){
+bool_e collision(uint32_t xc,uint32_t y,uint32_t r){
 
-	int r=4;
-	
+
 	/**
 	 * détecte si la distance entre centre du cercle et centre du carré est inférieure
 	 * au rayon du cercle et la moitié du côté du carré
 	 */
-
-	uint32_t distance = sqrt(square((200-x))+square((300-y)));		//à modifier
+	uint32_t distance = sqrt(square((xJoueur2-xc))+square((yJoueur2-y)));
 	if (distance<=(r+4)){
 
 
@@ -238,22 +327,139 @@ bool_e collision(uint16_t x,uint16_t y){
 	}
 
 	return FALSE;
+
 }
-//Fonctions	Buzzer
+bool_e collision2(uint32_t xc,uint32_t y,uint32_t r){
 
-void buzzer(bool_e b)
-{
-	if(b)
-	{
-		writeBUZZER(1);
 
-		HAL_Delay(500);			//test buzzer mais ça marche pas
+	/**
+	 * détecte si la distance entre centre du cercle et centre du carré est inférieure
+	 * au rayon du cercle et la moitié du côté du carré
+	 */
+	uint32_t distance = sqrt(square((xJoueur-xc))+square((yJoueur-y)));
+	if (distance<=(r+4)){
 
-		writeBUZZER(0);
+
+		return TRUE;
+
+	}
+
+	return FALSE;
+
+}
+void shot(uint16_t xc, uint16_t y){
+	ILI9341_DrawFilledCircle(xc, y+4,r, ILI9341_COLOR_BLACK);
+	ILI9341_DrawFilledCircle(xc, y-2,r, ILI9341_COLOR_RED);
+}
+
+void shot2(uint16_t xc, uint16_t y){
+	ILI9341_DrawFilledCircle(xc, y-4,r, ILI9341_COLOR_BLACK);
+	ILI9341_DrawFilledCircle(xc, y+2,r, ILI9341_COLOR_RED);
+}
+
+/*
+ * @Brief mode deux joueurs, chaqu'un controle un personnage
+ * @param  Non
+ * @retval TRUE pour victoire et FALSE pour defaite
+*/
+
+bool_e Mode_2_Joueurs(void){
+	uint32_t xc;
+	uint32_t xc2;
+	uint32_t y;
+	y=yJoueur;
+	uint32_t y2=yJoueur2;
+	int life1=3;
+	int life2=3;
+	int life1Before=3;
+	int life2Before=3;
+	drawLife(life1,ILI9341_COLOR_BLUE);
+	drawLife2(life1,ILI9341_COLOR_RED);
+	while (life1!=0 && life2!= 0){
+		JOYSTICK_move_x();
+		JOYSTICK2_move_x();
+		if(readButton() || tir==TRUE ){
+			if(tir==FALSE){
+				xc=xJoueur;
+				tir=TRUE;
+			}
+			shot(xc,y);
+			if(y<=20){
+				tir=FALSE;
+				ILI9341_DrawFilledCircle(xc, y+2,r+2, ILI9341_COLOR_BLACK);
+				y=yJoueur;
+				}
+				if (collision(xc,y,10)){
+					tir=FALSE;
+					y=yJoueur;
+					life2--;
+				}
+				if (life2<life2Before){
+					drawLife2(life2,ILI9341_COLOR_RED);
+					life2Before=life2;
+				}
+				y--;
+		}
+		if(readButton2() || tir2==TRUE ){
+					if(tir2==FALSE){
+						xc2=xJoueur2;
+						tir2=TRUE;
+					}
+					shot2(xc2,y2);
+					if(y2>=310){
+						tir2=FALSE;
+						ILI9341_DrawFilledCircle(xc2, y2+2,r+2, ILI9341_COLOR_BLACK);
+						y2=yJoueur2;
+						}
+						if (collision2(xc2,y2,10)){
+							tir2=FALSE;
+							life1--;
+							y2=yJoueur2;
+						}
+						if (life1<life1Before){
+							drawLife(life1,ILI9341_COLOR_BLUE);
+							life1Before=life1;
+						}
+						y2++;
+				}
+	}
+	if (life1==0){
+		return TRUE;
+	}
+	if (life2==0){
+		return FALSE;
 	}
 }
 
-//Fonctions
+/*
+ * @Brief affiche les vies d'un cote de l'ecran
+ * @param entier : nombre de vies entier : couleur
+ * @retval Non
+*/
+
+void drawLife(int nbLife, uint16_t color){
+	for (int i=0; i<3 ; i++){
+		ILI9341_DrawFilledRectangle(230, 230-i*100, 228,  270-i*100,ILI9341_COLOR_BLACK );
+	}
+	for (int i=0; i<nbLife ; i++){
+		ILI9341_DrawFilledRectangle(230, 230-i*100, 228,  270-i*100,color);
+	}
+}
+
+/*
+ * @Brief affiche les vies d'un cote de l'ecran
+ * @param entier : nombre de vies entier : couleur
+ * @retval Non
+*/
+
+void drawLife2(int nbLife, uint16_t color){
+	for (int i=0; i<3 ; i++){
+		ILI9341_DrawFilledRectangle(10, 230-i*100, 20,  270-i*100,ILI9341_COLOR_BLACK );
+	}
+	for (int i=0; i<nbLife ; i++){
+		ILI9341_DrawFilledRectangle(10, 230-i*100, 12,  270-i*100,color);
+	}
+}
 
 int main(void)
 {
@@ -264,24 +470,32 @@ int main(void)
 	//Initialisation de l'UART2 à la vitesse de 115200 bauds/secondes (92kbits/s) PA2 : Tx  | PA3 : Rx.
 		//Attention, les pins PA2 et PA3 ne sont pas reliées jusqu'au connecteur de la Nucleo.
 		//Ces broches sont redirigées vers la sonde de débogage, la liaison UART étant ensuite encapsulée sur l'USB vers le PC de développement.
-	UART_init(UART2_ID,115200);
+	//UART_init(UART2_ID,115200);
 
 	//"Indique que les printf sortent vers le périphérique UART2."
-	SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
+	//SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
 
 	//Initialisation du port de la led Verte (carte Nucleo)
 	BSP_GPIO_PinCfg(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH);
+
+	//Initialisation du port du bouton bleu (carte Nucleo)
+	BSP_GPIO_PinCfg(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN, GPIO_MODE_INPUT,GPIO_PULLUP,GPIO_SPEED_FREQ_HIGH);
 
 	//On ajoute la fonction process_ms à la liste des fonctions appelées automatiquement chaque ms par la routine d'interruption du périphérique SYSTICK
 	Systick_add_callback_function(&process_ms);
 
 	BSP_GPIO_PinCfg(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH);
 
+
+		//Initialisation du port du bouton bleu (carte Nucleo)
+	BSP_GPIO_PinCfg(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN, GPIO_MODE_INPUT,GPIO_PULLUP,GPIO_SPEED_FREQ_HIGH);
 	JOYSTICK_x_init();
 	ILI9341_Init();
 	ILI9341_Rotate(ILI9341_Orientation_Portrait_2);
 	ILI9341_Fill(ILI9341_COLOR_BLACK);
-	ILI9341_DrawCircle(120,20,5,ILI9341_COLOR_BLUE);
+	//Ecran_titre_jeu();
+	//Ecran_Selection();
+	//JOYSTICK2_move_x();
 	//JOYSTICK_move_x();
 	while (1){
 		state_machine();
@@ -335,7 +549,7 @@ void state_machine(void)
 			state = JOUEUR2;
 				}
 		break;
-		
+
 		case JOUEUR1:
 						// 1 seul joueur
 		//j1 = Mode_1_joueurs();
@@ -345,35 +559,38 @@ void state_machine(void)
 		state = DEFAITE;
 
 		break;
-		
+
 		case JOUEUR2:
 						// 2 joueurs
-
-		//j2 = Mode_2_joueurs();
+		FOND_Espace();
+		j2 = Mode_2_Joueurs();
 		if(j2)
 			{
 			state = VICTOIRE;
 			}
-		state = DEFAITE;
+		else
+			{
+			state = DEFAITE;
+			}
 
 		break;
-		
+
 		case DEFAITE:
 						//perd
 		Ecran_defaite();
 		state = MENU;
 		break;
-		
+
 		case VICTOIRE:
 						//gagne
 		Ecran_victoire();
 		state = MENU;
 		break;
-		
+
 		case OFF:
 		ILI9341_DisplayOff();
 		break;
-		
+
 		default:
 	break;
 	}
